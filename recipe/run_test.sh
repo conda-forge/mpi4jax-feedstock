5f98eb216ab17aeea5218ff2cd686c1874d68692
Min RK <benjaminrk@gmail.com>
2024-06-04 14:52:14 +0200

    remove unnecessary env for ompi 5

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -7,17 +7,6 @@ echo "# Running run_test.sh from directory: $(pwd)"
 echo "the content of this directory is:"
 ls .
 
-export OMPI_MCA_plm=isolated
-export OMPI_MCA_btl=self,tcp
-export OMPI_MCA_btl_vader_single_copy_mechanism=none
-export OMPI_MCA_rmaps_base_oversubscribe=yes
-
-if [ $(uname) == Darwin ]; then
-	echo "127.0.0.1 $(sudo scutil --get LocalHostName).local" | sudo tee -a /etc/hosts
-	cat /etc/hosts
-	sleep 10
-fi
-
 echo "###########################################################"
 echo "#############  Executing single-proc pytest   #############"
 echo "###########################################################"
@@ -25,6 +14,6 @@ echo "###########################################################"
 pytest .
 
 echo "###########################################################"
-echo "#############  Executing mpirun -np2 pytest   #############"
+echo "#############  Executing mpiexec -n 2 pytest   #############"
 echo "###########################################################"
-mpirun -host localhost:2 -v -np 2 pytest .
+mpiexec -n 2 pytest .
